902367369bc727475430fb3666dad3e16544f265
Daniel Trondoli <trondoli1@hotmail.com>
2021-09-12 19:54:54 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,10 +1,33 @@
+import { Saller } from "./seller"
+
 export type SaleSum = {
     sellerName: string;
     sum: number;
 }
 
-export type SaleSuccess ={
+export type SaleSuccess = {
     sellerName: string;
     visited: number;
     deals: number;
+}
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    saller: Saller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last?: boolean;
+    totalPages?: number;
+    totalElements?: number;
+    numberOfElements?: number;
+    first?: boolean;
+    size?: number;
+    number?: number;
+    empty?: boolean;
 }
