0cb8b01fd5d30609a178e8fd7f18cb5f7e300c40
Daniel Trondoli <trondoli1@hotmail.com>
2021-09-12 18:40:23 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,8 @@
-import BarCharts from "components/BarCharts";
-import DataTable from "components/DataTable";
-import DonutCharts from "components/DonutCharts";
-import Footer from "components/Footer";
-import NavBar from "components/Navbar";
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">Dash Board</h1>
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secundary">Taxa de Sucesso</h5>
-            <BarCharts />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secundary">Taxa de Sucesso</h5>
-            <DonutCharts />
-          </div>
-        </div>
-      </div>
-      <div className="py-3">
-        <h2 className="text-primary">Todas as Vendas</h2>
-      </div>
-      <DataTable />
-      <Footer />
-    </>
+    <Routes />
   );
 }
 
---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,31 @@
+import BarCharts from "components/BarCharts";
+import DataTable from "components/DataTable";
+import DonutCharts from "components/DonutCharts";
+import Footer from "components/Footer";
+import NavBar from "components/Navbar";
+
+const Dashboard = () => {
+    return (<>
+        <NavBar />
+        <div className="container">
+            <h1 className="text-primary py-3">Dash Board</h1>
+            <div className="row px-3">
+                <div className="col-sm-6">
+                    <h5 className="text-center text-secundary">Taxa de Sucesso</h5>
+                    <BarCharts />
+                </div>
+                <div className="col-sm-6">
+                    <h5 className="text-center text-secundary">Taxa de Sucesso</h5>
+                    <DonutCharts />
+                </div>
+            </div>
+        </div>
+        <div className="py-3">
+            <h2 className="text-primary">Todas as Vendas</h2>
+        </div>
+        <DataTable />
+        <Footer />
+    </>
+    )
+}
+export default Dashboard;
